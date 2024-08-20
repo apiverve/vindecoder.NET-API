d2435f451e1ec943ea5da51c45bb7ee5f01c8165
APIVerve Team <hello@apiverve.com>
2024-08-20 11:14:30 -0500

    Bug Fixes

--- VINDecoderAPIClient.cs
+++ VINDecoderAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, vindecoderQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, VINDecoderQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(vindecoderQueryOptions options = null)
+        public ResponseObj Execute(VINDecoderQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(vindecoderQueryOptions options)
+        private string constructURL(VINDecoderQueryOptions options)
         {
             string url = _apiEndpoint;
 
